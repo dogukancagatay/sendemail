9df22a0b5273e73b356614fa16046da9ee140fba
Dogukan Cagatay <dcagatay@gmail.com>
2014-02-09 17:15:02 +0200

    licence moved

--- sendemail.py
+++ sendemail.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python
-# This Source Code is subject to the terms of the Mozilla Public License
-# version 2.0 (the "License"). You can obtain a copy of the License at
-# http://mozilla.org/MPL/2.0/.
 
 #########################
 #
-# This script is intented to be used on systems which doesn't have a
-# proper MTA or any other sendmail program. It is a simple tool for sending emails
-# from inside scripts in headless environments  without installing any other tool
-# other than python itself which is coming standard in most of the *nix environments.
-#
-# It should be working on any system with Python v2.6.3+.
-# It is currently being used on the OSs below.
-# FreeBSD 8, Debian 6/7, Ubuntu 12.04/13.04, Mac OS X 10.9, and CentOS 6.4 systems.
+# This Source Code is subject to the terms of the Mozilla Public License
+# version 2.0 (the "License"). You can obtain a copy of the License at
+# http://mozilla.org/MPL/2.0/.
 #
 # Author: Dogukan Cagatay <dcagatay@gmail.com>
 # February 8, 2014
